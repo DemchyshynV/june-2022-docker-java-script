a24e5fe3ce3cccbc01121edf8e607bf16778fd72
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-12-23 21:48:16 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const App = () => {
+    const [users, setUser] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/users').then(value => setUser(value.data))
+    }, [])
+
+    return (
+        <div>
+            <h1>Users:</h1>
+            {users.map(value => <div key={value._id}>{value.name}</div>)}
+        </div>
+    );
+};
+
+export {App};
